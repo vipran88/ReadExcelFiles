2ab6f0bfd7b2b33e4803b5a0bc00edf4e20f3bcb
vipran88 <161015059+vipran88@users.noreply.github.com>
2024-09-06 07:31:00 +0530

    Update Program.cs

--- ReadExcelFiles/Program.cs
+++ ReadExcelFiles/Program.cs
@@ -1,8 +1,9 @@
-﻿using System;
+using System;
 using System.Data.SqlClient;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Spreadsheet;
 using System.Linq;
+using System.IO;
 
 class Program
 {
@@ -10,63 +11,93 @@ class Program
     {
 
         string directoryPath = "C:\\Users\\vippr\\source\\repos\\ReadExcelFiles\\ReadExcelFiles\\ReadExcel\\";
+        string connectionString = "Server=(localdb)\\MSSQLLocalDB;Database=ExcelDB;Trusted_Connection=True;";
+
 
         DirectoryInfo di = new DirectoryInfo(directoryPath);
 
-        
+        var subdirectories = di.EnumerateDirectories();
 
-        string excelFilePath = @"C:\Path\To\Your\ExcelFile.xlsx";
-        string connectionString = "Server=YourServerName;Database=YourDatabaseName;Trusted_Connection=True;";
-
-        try
+        foreach (var subdir in subdirectories)
         {
-            using (SpreadsheetDocument spreadsheetDocument = SpreadsheetDocument.Open(excelFilePath, false))
+
+            var files = subdir.EnumerateFiles();
             {
-                WorkbookPart workbookPart = spreadsheetDocument.WorkbookPart!;
-                WorksheetPart worksheetPart = workbookPart.WorksheetParts.First();
-                SheetData sheetData = worksheetPart.Worksheet.Elements<SheetData>().First();
 
-                using (SqlConnection connection = new SqlConnection(connectionString))
+                foreach (var file in files) {
+
+                    ReadExcel(file.FullName, connectionString);
+
+
+                }
+
+            }
+
+        }
+
+
+
+
+
+        Console.WriteLine("Data import completed.");
+
+
+         void ReadExcel(string excelFilePath, string connectionString)
+        {
+
+
+            try
+            {
+                using (SpreadsheetDocument spreadsheetDocument = SpreadsheetDocument.Open(excelFilePath, false))
                 {
-                    connection.Open();
+                    WorkbookPart workbookPart = spreadsheetDocument.WorkbookPart!;
+                    WorksheetPart worksheetPart = workbookPart.WorksheetParts.First();
+                    SheetData sheetData = worksheetPart.Worksheet.Elements<SheetData>().First();
 
-                    // Skip the header row
-                    foreach (Row row in sheetData.Elements<Row>().Skip(1))
+                    using (SqlConnection connection = new SqlConnection(connectionString))
                     {
-                        string query = "INSERT INTO YourTableName (Column1, Column2, Column3) VALUES (@val1, @val2, @val3)";
-                        using (SqlCommand command = new SqlCommand(query, connection))
-                        {
-                            command.Parameters.AddWithValue("@val1", GetCellValue(workbookPart, row.Elements<Cell>().ElementAtOrDefault(0)));
-                            command.Parameters.AddWithValue("@val2", GetCellValue(workbookPart, row.Elements<Cell>().ElementAtOrDefault(1)));
-                            command.Parameters.AddWithValue("@val3", GetCellValue(workbookPart, row.Elements<Cell>().ElementAtOrDefault(2)));
+                        connection.Open();
 
-                            command.ExecuteNonQuery();
+                        // Skip the header row
+                        foreach (Row row in sheetData.Elements<Row>().Skip(1))
+                        {
+                            string query = "INSERT INTO ExcelToTable (Column1, Column2, Column3) VALUES (@val1, @val2, @val3)";
+                            using (SqlCommand command = new SqlCommand(query, connection))
+                            {
+                                command.Parameters.AddWithValue("@val1", GetCellValue(workbookPart, row.Elements<Cell>().ElementAtOrDefault(0)));
+                                command.Parameters.AddWithValue("@val2", GetCellValue(workbookPart, row.Elements<Cell>().ElementAtOrDefault(1)));
+                                command.Parameters.AddWithValue("@val3", GetCellValue(workbookPart, row.Elements<Cell>().ElementAtOrDefault(2)));
+
+                                command.ExecuteNonQuery();
+                            }
                         }
                     }
                 }
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+
+
         }
-        catch (Exception ex)
+
+
+         static object GetCellValue(WorkbookPart workbookPart, Cell? cell)
         {
-            Console.WriteLine($"An error occurred: {ex.Message}");
+            if (cell == null)
+                return DBNull.Value;
+
+            string? value = cell.InnerText;
+
+            if (cell.DataType != null && cell.DataType.Value == CellValues.SharedString)
+            {
+                return workbookPart.SharedStringTablePart!.SharedStringTable.Elements<SharedStringItem>()
+                    .ElementAt(int.Parse(value)).InnerText;
+            }
+
+            return value;
         }
 
-        Console.WriteLine("Data import completed.");
-    }
-
-    private static object GetCellValue(WorkbookPart workbookPart, Cell? cell)
-    {
-        if (cell == null)
-            return DBNull.Value;
-
-        string? value = cell.InnerText;
-
-        if (cell.DataType != null && cell.DataType.Value == CellValues.SharedString)
-        {
-            return workbookPart.SharedStringTablePart!.SharedStringTable.Elements<SharedStringItem>()
-                .ElementAt(int.Parse(value)).InnerText;
-        }
-
-        return value;
     }
 }
